30fc75aaabbb10d00558c7e35bd7fc66ece901a3
Richard Ingwe <richardingwe5@gmail.com>
2023-02-08 12:56:49 +0100

    add: footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+  import Footer from "./components/Footer.svelte";
   import Header from "./components/Header.svelte";
 
   
 </script>
 
+<Header/>
 <main>
-  <Header/>
   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur neque impedit repellendus accusantium cumque laboriosam sapiente, a iste, provident fugiat itaque placeat nihil tempore ad eius illo aperiam exercitationem maxime!</p>
 </main>
+<Footer/>
 
 <style>
   main {
