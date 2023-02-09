5ebaf2924c6ffb12605dff0527693c89ef933b17
Richard Ingwe <richardingwe5@gmail.com>
2023-02-09 04:45:54 +0100

    poll list

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,45 @@
-<script lang="ts">
-  import CreatePollForm from "./components/CreatePollForm.svelte";
-  import Footer from "./components/Footer.svelte";
-  import Header from "./components/Header.svelte";
-
+<script>
+	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
+	import CreatePollForm from './components/CreatePollForm.svelte';
+	import PollList from './components/PollList.svelte';
   import Tabs from './shared/Tabs.svelte';
   
   // tabs
   let items = ['Current Polls', 'Add New Poll'];
   let activeItem = 'Current Polls';
-  const tabChange = (e: { detail: string; }) => activeItem = e.detail;
+  const tabChange = (e) => activeItem = e.detail;
+  // polls
+  let polls = [
+    {
+      id: 1,
+      question: 'Python or JavaScript?',
+      answerA: 'Python',
+      answerB: 'JavaScript',
+      votesA: 9,
+      votesB: 15,
+    },
+  ];
+  const handleAdd = (e) => {
+    const poll = e.detail;
+    polls = [poll, ...polls];
+    activeItem = 'Current Polls';
+  }
 </script>
 
-<Header/>
+<Header />
 <main>
   <Tabs {activeItem} {items} on:tabChange={tabChange} />
   {#if activeItem === 'Current Polls'}
-    <p>poll list goes here</p>
+    <PollList {polls} />
   {:else if activeItem === 'Add New Poll'}
-    <CreatePollForm />
+    <CreatePollForm on:add={handleAdd} />
   {/if}
 </main>
-<Footer/>
+<Footer />
 
 <style>
-  main {
+	main{
     width: 100%;
     max-width: 960px;
     margin: 40px auto;
