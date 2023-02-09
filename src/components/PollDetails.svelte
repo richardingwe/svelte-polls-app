8aa2a8b4f81af12233ececffae53908f0f40eb81
Richard Ingwe <richardingwe5@gmail.com>
2023-02-09 05:01:52 +0100

    vote bars

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -5,8 +5,10 @@
   const dispatch = createEventDispatcher();
   // reactive values
   $: totalVotes = poll.votesA + poll.votesB;
+  $: percentA = Math.floor(100 / totalVotes * poll.votesA) || 0;
+  $: percentB = Math.floor(100 / totalVotes * poll.votesB) || 0;
   // handling votes
-  const handleVote = (option: string, id:string) => {
+  const handleVote = (option, id) => {
     dispatch('vote', {option, id});
   };
 </script>
@@ -17,12 +19,12 @@
     <p>Total votes: { totalVotes }</p>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="answer" on:click={() => handleVote('a', poll.id)}>
-      <div class="percent percent-a"></div>
+      <div class="percent percent-a" style="width: {percentA}%"></div>
       <span>{ poll.answerA } ({ poll.votesA } votes)</span>
     </div>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="answer" on:click={() => handleVote('b', poll.id)}>
-      <div class="percent percent-b"></div>
+      <div class="percent percent-b" style="width: {percentB}%"></div>
       <span>{ poll.answerB } ({ poll.votesB } votes)</span>
     </div>
   </div>
@@ -51,5 +53,18 @@
   span{
     display: inline-block;
     padding: 10px 20px;
+  }
+  .percent{
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+  }
+  .percent-a{
+    background: rgba(217,27,66,0.2);
+    border-left: 4px solid #d91b42;
+  }
+  .percent-b{
+    background: rgba(69,196,150,0.2);
+    border-left: 4px solid #45c496;
   }
 </style>
