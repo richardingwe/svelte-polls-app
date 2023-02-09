73ecde2370f61da6c0da7ace2f1a30bdedc26251
Richard Ingwe <richardingwe5@gmail.com>
2023-02-09 04:38:17 +0100

    add form validation

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,10 +1,35 @@
 <script>
-  import Button from "../shared/Button.svelte";
-
-
+  import Button from '../shared/Button.svelte';
   let fields = { question: '', answerA: '', answerB: '' };
-  let submitHandler = () => {
-    console.log(fields);
+  let errors = { question: '', answerA: '', answerB: '' };
+  let valid = false;
+  const submitHandler = () => {
+    valid = true;
+    // question
+    if (fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = 'Question must be at least 5 chars long'
+    } else {
+      errors.question = ''
+    }
+    // answer A
+    if (fields.answerA.trim().length < 1) {
+      valid = false;
+      errors.answerA = 'Answer A cannot be empty'
+    } else {
+      errors.answerA = ''
+    }
+    // answer B
+    if (fields.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = 'Answer B cannot be empty'
+    } else {
+      errors.answerB = ''
+    }
+    // add new poll
+    if (valid) {
+      console.log('valid', fields);
+    }
   }
 </script>
 
@@ -12,16 +37,19 @@
   <div class="form-field">
     <label for="question">Poll Question:</label>
     <input type="text" id="question" bind:value={fields.question}>
+    <div class="error">{ errors.question }</div>
   </div>
   <div class="form-field">
     <label for="answer-a">Answer A value:</label>
     <input type="text" id="answer-a" bind:value={fields.answerA}>
+    <div class="error">{ errors.answerA }</div>
   </div>
   <div class="form-field">
     <label for="answer-b">Answer B value:</label>
     <input type="text" id="answer-b" bind:value={fields.answerB}>
+    <div class="error">{ errors.answerB }</div>
   </div>
-  <Button type='secondary' flat={true}>Add Poll</Button>
+  <Button>Add Poll</Button>
 </form>
 
 <style>
@@ -43,5 +71,11 @@
   label{
     margin: 10px auto;
     text-align: left;
+  }
+  
+  .error{
+    font-weight: bold;
+    font-size: 12px;
+    color: #d91b42;
   }
   </style>
